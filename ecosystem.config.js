c93d4f1781f1949258f30490b74b24d9a08c2fc1
root <root@srv1791781.hstgr.cloud>
2026-06-30 09:14:41 +0000

    (VPS) Added pm2 ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: 'madeirense-api',
+      cwd: '/var/www/madeirense/packages/api',
+      script: 'node',
+      args: 'dist/server.js',
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 3000,
+      },
+    },
+  ],
+}
