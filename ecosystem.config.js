1643f5246fe5da3f9b057a7a759daef689701528
deploy (VPS) <deploy@187.124.219.40>
2026-07-01 23:04:49 +0000

    Updated config with missing node parameters

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: 'madeirense-api',
       cwd: '/var/www/madeirense/packages/api',
       script: 'node',
-      args: 'dist/server.js',
+      args: '--env-file=.env dist/server.js',
       env_production: {
         NODE_ENV: 'production',
         PORT: 3001,
